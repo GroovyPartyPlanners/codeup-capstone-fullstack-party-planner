cdedab3f6e33f7ed231a91bc40a5bc46af1917a1
Stout13 <stout.stephen13@gmail.com>
2022-07-12 14:30:44 -0500

    initial commit

---
+++ src/main/java/com/codeup/partygate/PartyGateApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.partygate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyGateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartyGateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/partygate/PartyGateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.partygate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyGateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
