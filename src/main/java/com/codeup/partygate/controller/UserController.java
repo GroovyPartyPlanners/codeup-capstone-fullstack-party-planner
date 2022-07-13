8f69cf179b3798360c27fa238602ea6ea8f53e41
Stout13 <stout.stephen13@gmail.com>
2022-07-13 10:23:50 -0500

    login

--- src/main/java/com/codeup/partygate/controller/UserController.java
+++ src/main/java/com/codeup/partygate/controller/UserController.java
@@ -7,7 +7,18 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     @GetMapping(path = "/user")
-    public String showUser() {
+    public String userShow() {
         return "user";
+    }
+
+    @PostMapping(path = "/user/login")
+    public String userLogin() {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        if (username.equals("admin") && password.equals("password")) {
+            response.sendRedirect("/profile");
+        } else{
+            response.sendRedirect("/login");
+        }
     }
 }
