437f0575ba28aa735e761ae8c276812af30a46f1
David Prieto <david.s.prieto1@gmail.com>
2022-07-13 16:11:42 -0500

    Created Views Pages

--- src/main/java/com/codeup/partygate/models/User.java
+++ src/main/java/com/codeup/partygate/models/User.java
@@ -1,5 +1,122 @@
 package com.codeup.partygate.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(length = 50, nullable = false)
+    private String first_name;
+
+    @Column(length = 50, nullable = false)
+    private String last_name;
+
+    @Column(length = 75, nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 50, nullable = false, unique = true)
+    private String username;
+
+    @Column(length = 75, nullable = true)
+    private String group_name;
+
+    @Column(length = 60, nullable = false)
+    private String password;
+
+    @Column(length = 100, nullable = true)
+    private String user_pic_url;
+
+////    NEW CONSTRUCTOR for authentication process (login/logout)
+//    public User(User copy) {
+//        id = copy.id; // This line is SUPER important! Many things won't work if it's absent
+//        first_name = copy.first_name;
+//        last_name = copy.last_name;
+//        email = copy.email;
+//        username = copy.username;
+//        group_name = copy.group_name;
+//        password = copy.password;
+//        user-pic-url = copy.user_pic_url;
+//    }
+
+    public User () {}
+
+    public User(long id, String first_name, String last_name, String email, String username, String group_name, String password, String user_pic_url) {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.username = username;
+        this.group_name = group_name;
+        this.password = password;
+        this.user_pic_url = user_pic_url;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getGroup_name() {
+        return group_name;
+    }
+
+    public void setGroup_name(String group_name) {
+        this.group_name = group_name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUser_pic_url() {
+        return user_pic_url;
+    }
+
+    public void setUser_pic_url(String user_pic_url) {
+        this.user_pic_url = user_pic_url;
+    }
 }
