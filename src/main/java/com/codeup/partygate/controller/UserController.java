78dfb824b0e57631cb1b60ca5574449a541ef4e6
Stout13 <stout.stephen13@gmail.com>
2022-07-13 13:58:35 -0500

    login

--- src/main/java/com/codeup/partygate/controller/UserController.java
+++ src/main/java/com/codeup/partygate/controller/UserController.java
@@ -1,13 +1,44 @@
 package com.codeup.partygate.controller;
 
+import com.codeup.partygate.model.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+
+
+//import com.codeup.partygate.model.User;  ==========================
+//import com.codeup.partygate.model.PostRepository;
+
 
 @Controller
 public class UserController {
 
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @GetMapping(path = "/user")
-    public String showUser() {
+    public String userShow() {
         return "user";
     }
-}
+
+    @PostMapping(path= "/user/create")
+    public String userLogin() {
+    }
+    }
+
+
+//    @PostMapping(path = "/user/login")
+//    public String userLogin() {
+//        String username = request.getParameter("username");
+//        String password = request.getParameter("password");
+//        if (username.equals("admin") && password.equals("password")) {
+//            response.sendRedirect("/profile");
+//        } else{
+//            response.sendRedirect("/login");
+//        }
+//        return "event";
+//    }
+//}
--- src/main/java/com/codeup/partygate/model/User.java
+++ src/main/java/com/codeup/partygate/model/User.java
@@ -1,4 +1,46 @@
 package com.codeup.partygate.model;
 
+import javax.persistence.*;
+
+@Table(name="user")
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public long user_id;
+
+    @Column
+    public String username;
+
+    @Column
+    public String description;
+
+    public Post(){
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
