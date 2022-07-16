26f9871939c969a66f5d33305eacee524d509ac4
Stout13 <stout.stephen13@gmail.com>
2022-07-16 09:06:18 -0500

    party redirect

--- src/main/java/com/codeup/partygate/controllers/PartyController.java
+++ src/main/java/com/codeup/partygate/controllers/PartyController.java
@@ -2,6 +2,7 @@ package com.codeup.partygate.controllers;
 
 import com.codeup.partygate.models.Party;
 import com.codeup.partygate.repositories.PartyRepository;
+import org.apache.catalina.connector.Request;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.Objects;
 
@@ -30,27 +33,34 @@ public class PartyController {
     }
 
     @PostMapping(path = "/parties")
-    public String saveParty(@ModelAttribute Party party){
+    public String saveParty(@ModelAttribute Party party, HttpServletRequest request){
 //        if (party.getEvent_id() == null) {
 //            party.setEvent_id("0");
 //        }
         partyRepository.save(party);
-        return "redirect:/parties";
+        return "views/parties";
     }
 
-    @GetMapping(path = "/party/{name}")
-    public String showParty(@PathVariable String name, Model model) {
+    @GetMapping(path = "/party/{id}")
+    public void showParty(@PathVariable Long id, Model model, Request request) {
         ArrayList<Party> parties = (ArrayList<Party>) partyRepository.findAll();
         Party thisParty = null;
         for (Party party : parties) {
-            if (Objects.equals(party.getParty_name(), name)) {
+            if (Objects.equals(party.getParty_id(), id)) {
                 thisParty = party;
             }
         }
         model.addAttribute("party", thisParty);
-        String html = "redirect: /views/party/{" + thisParty.getParty_name()+ "}";
+        HttpSession session = request.getSession();
+        session.setAttribute("party_id", request.getParameter("name"));
+//        assert thisParty != null;
+//        String html = "/views/party/{" + id + "}";
+//        return html;
+    }
 
-        return html;
+    @GetMapping(path = "/party")
+    public String showNoParty() {
+        return "/views/party";
     }
 
 
@@ -66,4 +76,7 @@ public class PartyController {
         String html = "redirect: /party/{" + party.getParty_name()+ "}";
         return html;
     }
+
+//    @PostMapping(path = "/party/select")
+//    public String
 }
