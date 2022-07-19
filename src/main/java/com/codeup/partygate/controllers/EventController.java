815fd8322bfc37c826dbcf50dafb7e6fdc02a305
David Prieto <david.s.prieto1@gmail.com>
2022-07-18 20:46:35 -0500

    Form validation

--- src/main/java/com/codeup/partygate/controllers/EventController.java
+++ src/main/java/com/codeup/partygate/controllers/EventController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class EventController {
 
-    @GetMapping(path = "/event")
+    @GetMapping( "/event")
     public String showEvent() {
         return "views/event";
     }
