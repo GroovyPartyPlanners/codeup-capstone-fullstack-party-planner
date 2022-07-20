901bbe81034ba9e34243e7e3c2fce625be7097bc
Stout13 <stout.stephen13@gmail.com>
2022-07-19 21:45:43 -0500

    join party(broken)

--- src/main/java/com/codeup/partygate/controllers/PartyController.java
+++ src/main/java/com/codeup/partygate/controllers/PartyController.java
@@ -13,29 +13,45 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 @Controller
 public class PartyController {
 
-    private final PartyRepository partyRepository;
-//    private final UserRepository userRepository;
+    public final PartyRepository partyRepository;
+    public final UserRepository userRepository;
 
     public PartyController(PartyRepository partyRepository, UserRepository userRepository) {
-//        this.userRepository = userRepository;
+        this.userRepository = userRepository;
         this.partyRepository = partyRepository;
     }
 
+    //  delivers the party-creation form which allows the creation of new parties
+    @GetMapping(path = "/party-form")
+    public String partyForm (Model model) {
+
+//        System.out.println("Stuff is going to happen");
+        model.addAttribute("party", new Party());
+        return "/views/party-form";
+    }
+
+//    Shows all the parties in an index
+//    Creates a new, necessary party object that
+//    is included in the anchor for each party
+//    which allows navigation to /party-select/{id}, the individual party page
     @GetMapping(path = "/parties")
-    public String showPartyForm(Model model) {
+    public String partiesShow(Model model) {
         model.addAttribute("party", new Party());
         ArrayList<Party> parties = (ArrayList<Party>) partyRepository.findAll();
         model.addAttribute("parties", parties);
 
         return "views/parties";
     }
-
+//    allows the storing of new party details
+//    redirects to party index page
     @PostMapping(path = "/party/save")
-    public String saveParty(@ModelAttribute Party party) {
+    public String partySave(@ModelAttribute Party party) {
 //        if (party.getEvent_id() == null) {
 //            party.setEvent_id("0");
 //        }
@@ -52,20 +68,14 @@ public class PartyController {
 //    }
 
 
-    @GetMapping(path = "/party-form")
-    public String partyForm (Model model) {
+//
+//    @PostMapping(path = "/party-form")
+//    public String partyCreate (@ModelAttribute Party party) {
+//        partyRepository.save(party);
+//        return "views/home";
+//    }
 
-//        System.out.println("Stuff is going to happen");
-        model.addAttribute("party", new Party());
-        return "/views/party-form";
-    }
-
-    @PostMapping(path = "/party-form")
-    public String partyCreate (@ModelAttribute Party party) {
-        partyRepository.save(party);
-        return "views/home";
-    }
-
+//    uses party id
     @GetMapping(path = "/party-select/{id}")
     public String partyPage(@PathVariable long id, Model model) {
         User loggedUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
@@ -81,28 +91,34 @@ public class PartyController {
         return "views/party-select";
     }
 
-//    @GetMapping(path="/attend/{userId}/{partyId}")
-//    public String userAttend(@PathVariable long userId, long partyId) {
+    @GetMapping(path="/attend/{userId}/{partyId}")
+    public String userAttend(@PathVariable String userId, String partyId) {
         //        HOW TO JOIN TABLES
 //===========================================================================
-//        User user = userRepository.getById(userId);
-//        Party party = partyRepository.getById(partyId);
+        Long user = Integer.toUnsignedLong(Integer.parseInt(userId));
+        Long party = Integer.toUnsignedLong(Integer.parseInt(partyId));
+        User u = userRepository.getById(user);
+        Party p = partyRepository.getById(party);
 
 //        System.out.println("user.getUsername() = " + user.getUsername());
 //        System.out.println("party.getParty_name() = " + party.getParty_name());
 
-//        List<Party> parties = new ArrayList<>();
-//
-//        parties.add(party);
+        Set<Party> parties = new HashSet<>();
+//        List<User> users = new ArrayList<>();
+        parties.add(p);
+//        users.add(u);
 //===============
-
+//        p.setAttendees(users);
 //        user-party association
-//        user.setTailgateParties(parties);
-
-//        userRepository.save(user); //persist to the db
+        u.setTailgateParties(parties);
+//        System.out.println(party);
+//        System.out.println(user);
+//        partyRepository.save(p);
+        userRepository.save(u); //persist to the db
+        String redirect = "/views/party-select/" + partyId;
 //===========================================================================
-//        return "views/party";
-//    }
+        return redirect;
+    }
 }
 
 
