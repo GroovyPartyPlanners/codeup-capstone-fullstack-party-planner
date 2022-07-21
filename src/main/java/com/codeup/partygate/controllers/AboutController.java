691b1224b877cc07c7ed874295b14020a894c60f
David Prieto <david.s.prieto1@gmail.com>
2022-07-21 12:10:45 -0500

    merge conflict

--- src/main/java/com/codeup/partygate/controllers/AboutController.java
+++ src/main/java/com/codeup/partygate/controllers/AboutController.java
@@ -5,12 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
-// serves about us html
+
 @Controller
 public class AboutController {
 
-
-//    get map to display about.html
     @GetMapping("/about")
 //    shows about page with String
     public String showAbout(User user, Model model, Errors validation) {
