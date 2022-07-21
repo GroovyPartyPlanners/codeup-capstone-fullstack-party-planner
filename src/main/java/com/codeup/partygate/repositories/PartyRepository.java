c091758e02f83a36243bb713e1dcc640640cec82
Keaton Hutto <keatonhutto8@gmail.com>
2022-07-21 15:26:11 -0500

    deleted custom query

--- src/main/java/com/codeup/partygate/repositories/PartyRepository.java
+++ src/main/java/com/codeup/partygate/repositories/PartyRepository.java
@@ -2,11 +2,10 @@ package com.codeup.partygate.repositories;
 
 import com.codeup.partygate.models.Party;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface PartyRepository extends JpaRepository<Party, Long> {
-    @Query(nativeQuery = true, value = "SELECT * FROM partyplanner_db.parties WHERE user_id = ?")
+
     List<Party> findAllByUserId(long id);
 }
