77a233cadbec5d7cd5ed3d123fb3b95b9494fa06
David Prieto <david.s.prieto1@gmail.com>
2022-07-21 15:33:37 -0500

    Testing user comments

--- src/main/java/com/codeup/partygate/repositories/PartyRepository.java
+++ src/main/java/com/codeup/partygate/repositories/PartyRepository.java
@@ -2,11 +2,9 @@ package com.codeup.partygate.repositories;
 
 import com.codeup.partygate.models.Party;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface PartyRepository extends JpaRepository<Party, Long> {
-    
     List<Party> findAllByUserId(long id);
 }
