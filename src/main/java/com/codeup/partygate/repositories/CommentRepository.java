e8dec431c3d0e98b23b5fda786a208c649248925
David Prieto <david.s.prieto1@gmail.com>
2022-07-21 15:41:41 -0500

    testing user comments

--- src/main/java/com/codeup/partygate/repositories/CommentRepository.java
+++ src/main/java/com/codeup/partygate/repositories/CommentRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-//  @Query(nativeQuery = true, value = "SELECT * FROM partyplanner_db.comments WHERE party_id = ?")
     List<Comment> findAllByPartyId(long id);
 }
