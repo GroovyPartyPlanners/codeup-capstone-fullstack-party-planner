79363bb799ca8862d449222bb5b6c7f3a2d477f6
Keaton Hutto <keatonhutto8@gmail.com>
2022-07-21 14:46:29 -0500

    mapbpx

---
+++ src/main/resources/static/js/api.js
@@ -0,0 +1,4 @@
+const clientId = "Mjc4MTcxNDZ8MTY1NzU3OTE4MC4yMjE1NzI"
+const clientSecret = "client_secret=Mjc4MTcxNDZ8MTY1NzU3OTE4MC4yMjE1NzI&client_secret=cbd2......."
+const googleapi = 'AIzaSyBvkLTs_JIK818BW-_QWPtOTUFjOPJj1L8'
+const mapboxapi = 'pk.eyJ1Ijoia2VhdG9uaHV0dG8iLCJhIjoiY2wycWw3cWRnMDFwOTNqcGFwbDhqZTh6aCJ9.JA4KRbfaB02VWnaD8Ecs7g'
