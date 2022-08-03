51963befc10645bd59fd57f8af852558dd35b28c
Keaton Hutto <keatonhutto8@gmail.com>
2022-08-03 12:51:59 -0500

    api

--- src/main/resources/static/js/homepage.js
+++ src/main/resources/static/js/homepage.js
@@ -42,37 +42,6 @@ function windowLoading(){
                 console.log(lat, lng);
                 console.log(type)
 //dragging pin fetch
-
-        fetch(`https://api.seatgeek.com/2/events?lat=${lat}&lon=${long}&type=${type}&client_id=Mjc4MTcxNDZ8MTY1NzU3OTE4MC4yMjE1NzI`)
-            .then(response => response.json())
-            .then(data => {
-
-                var events ='';
-                var popularity = '';
-                total = document.getElementById('total');
-                total.innerHTML = `<h2>Total Events ${data.meta.total}</h2>`
-                for(var i = 0; i < data.events.length; i++) {
-                    const date = new Date(data.events[i].datetime_local);
-                    const [year, month, day] = [date.getFullYear(), date.getMonth() + 1, date.getDate()];
-                    const dateString = 'm/d/year'+" "+`${month}/${day}/${year}`;
-                    events += `<div class='loopEvent profile-div-outer row'>${data.events[i].title}\n`+" "+`${data.events[i].venue.name}\n`+" "+`${data.events[i].venue.display_location}\n`+" Date: "+ dateString + `${data.events[i].popularity}\n`+`<div class="row"><a href="/events/${data.events[i].id}" class="col profile-btn">Host a party at ${data.events[i].title}</a>`+
-                        `<a href="/parties/${data.events[i].id}" class="col profile-btn">Or see parties at ${data.events[i].title}</a></div></div></br></br>`
-                }
-
-                for(var i = 0; i < 1; i++) {
-                    if(data.meta.total <11){
-                        pages = document.getElementById('pages');
-                        pages.innerHTML = "";
-                    }
-                    if(data.meta.total>=11){
-                        pages = document.getElementById('pages');
-                        pages.innerHTML = "";
-                        let pagesCount = Math.ceil(data.meta.total/10);
-                        pageNum = document.getElementById('pageNum');
-                        for(let i = 0; i < pagesCount; i++){
-                            pageNum.innerHTML = 'Page:'
-                            pages.innerHTML += `<a href="#"  onclick="page(${i})">`+" "+`${i+1}</a>`
-
                 fetch(`https://api.seatgeek.com/2/events?lat=${lat}&lon=${lng}&type=${type}&client_id=Mjc4MTcxNDZ8MTY1NzU3OTE4MC4yMjE1NzI`)
                     .then(response => response.json())
                     .then(data => {
@@ -87,7 +56,6 @@ function windowLoading(){
                             const dateString = 'm/d/year'+" "+`${month}/${day}/${year}`;
                             events += `<div><h1 class='loopEvent'>${data.events[i].title}`+" "+`${data.events[i].venue.name}`+" "+`${data.events[i].venue.display_location}`+" Date: "+dateString+" Popularity "+ `${data.events[i].popularity}`+`<a href="/events/${data.events[i].id}">Host a party at ${data.events[i].title}</a></br>`+
                                 `<p class='loopEvent'><a href="/parties/${data.events[i].id}">Or see parties at ${data.events[i].title}</a></p></br></h1></div>`
-
                         }
 
                         for(var i = 0; i < 1; i++) {
@@ -140,38 +108,6 @@ function windowLoading(){
                                 `<h2 class='loopEvent'><a href="/parties/${data.events[i].id}">Or see parties at ${data.events[i].title}</a></h2></br></div>`
                         }
 
-
-//window onload
-    fetch(`https://api.seatgeek.com/2/events?lat=${lat}&lon=${lng}&type=comedy&client_id=Mjc4MTcxNDZ8MTY1NzU3OTE4MC4yMjE1NzI`)
-        .then(response => response.json())
-        .then(data => {
-                console.log(data);
-                console.log('on load')
-                var events ='';
-
-                total = document.getElementById('total');
-                total.innerHTML = `<h2 class='totalEvents'>Total Events ${data.meta.total}</h2>`
-                for(var i = 0; i < data.events.length; i++) {
-                    const date = new Date(data.events[i].datetime_local);
-                    const [year, month, day] = [date.getFullYear(), date.getMonth() + 1, date.getDate()];
-                    const dateString = `${month}/${day}/${year}`;
-                    events += `<div class='loopEvent profile-div-outer row'>${data.events[i].title}\n`+" "+`${data.events[i].venue.name}\n`+" "+`${data.events[i].venue.display_location}\n`+" Date: "+ dateString + `${data.events[i].popularity}\n`+`<div class="row"><a href="/events/${data.events[i].id}" class="col profile-btn">Host a party at ${data.events[i].title}</a>`+
-                        `<a href="/parties/${data.events[i].id}" class="col profile-btn">Or see parties at ${data.events[i].title}</a></div></div></br></br>`
-                }
-
-                for(var i = 0; i < 1; i++) {
-                    if(data.meta.total <11){
-                        console.log('no pages')
-                        pages = document.getElementById('pages');
-                        pages.innerHTML = "";
-                    }
-                    if(data.meta.total>=11){
-                        pages = document.getElementById('pages');
-                        let pagesCount = Math.ceil(data.meta.total/10);
-                        pages.innerHTML = "";
-                        for(let i = 0; i < pagesCount; i++){
-                            pages.innerHTML += `<a href="#" onclick="pin(${i})">`+" "+`${i+1}</a>`
-
                         for(var i = 0; i < 1; i++) {
                             if(data.meta.total <11){
                                 console.log('no pages')
@@ -186,7 +122,6 @@ function windowLoading(){
                                     pages.innerHTML += `<a href="#" onclick="page(${i})">`+" "+`${i+1}</a>`
                                 }
                             }
-
                         }
                         document.getElementById("eventTitle").innerHTML = events
                     }
@@ -195,35 +130,6 @@ function windowLoading(){
         }
 
         getLocation();
-
-
-    fetch(`https://api.seatgeek.com/2/events?q=${search}&type=${type}&client_id=Mjc4MTcxNDZ8MTY1NzU3OTE4MC4yMjE1NzI`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            var events ='';
-
-            total = document.getElementById('total');
-            total.innerHTML = `<h2>Total Events ${data.meta.total}</h2>`
-            for(var i = 0; i < data.events.length; i++) {
-                const date = new Date(data.events[i].datetime_local);
-                const [year, month, day] = [date.getFullYear(), date.getMonth() + 1, date.getDate()];
-                const dateString = 'm/d/year'+" "+`${month}/${day}/${year}`;
-                events += `<div class='loopEvent profile-div-outer row'>${data.events[i].title}\n`+" "+`${data.events[i].venue.name}\n`+" "+`${data.events[i].venue.display_location}\n`+" Date: "+ dateString + `${data.events[i].popularity}\n`+`<div class="row"><a href="/events/${data.events[i].id}" class="col profile-btn">Host a party at ${data.events[i].title}</a>`+
-                    `<a href="/parties/${data.events[i].id}" class="col profile-btn">Or see parties at ${data.events[i].title}</a></div></div></br></br>`
-            }
-            for(var i = 0; i < 1; i++) {
-                if(data.meta.total <11){
-                    pages = document.getElementById('pages');
-                    pages.innerHTML = "";
-                }
-                if(data.meta.total>=11){
-                    pages = document.getElementById('pages');
-                    let pagesCount = Math.ceil(data.meta.total/10);
-                    pages.innerHTML = "";
-
-                    for(let i = 0; i < pagesCount; i++){
-                        pages.innerHTML += `<a href="#"  onclick="page(${i})">`+" "+`${i+1}</a>`
 
 
 //submit button
@@ -263,29 +169,10 @@ function windowLoading(){
 
                             }
 
-                console.log(data);
-                console.log('page clicked!')
-                var events ='';
-                console.log(data.events);
-                total = document.getElementById('total');
-                total.innerHTML = `<h2>Total Events ${data.meta.total}</h2>`
-                for(var i = 0; i < data.events.length; i++) {
-                    const date = new Date(data.events[i].datetime_local);
-                    const [year, month, day] = [date.getFullYear(), date.getMonth() + 1, date.getDate()];
-                    const dateString = 'm/d/year'+" "+`${month}/${day}/${year}`;
-                    events += `<div class='loopEvent profile-div-outer row'>${data.events[i].title}\n`+" "+`${data.events[i].venue.name}\n`+" "+`${data.events[i].venue.display_location}\n`+" Date: "+ dateString + `${data.events[i].popularity}\n`+`<div class="row"><a href="/events/${data.events[i].id}" class="col profile-btn">Host a party at ${data.events[i].title}</a>`+
-                        `<a href="/parties/${data.events[i].id}" class="col profile-btn">Or see parties at ${data.events[i].title}</a></div></div></br></br>`
-                }
-                // for(var i = 0; i < 1; i++) {
-                //     if(data.meta.total>=11){
-                //         pages = document.getElementById('pages');
-                //         let pagesCount = Math.ceil(data.meta.total/10);
-
                         }
                         document.getElementById("eventTitle").innerHTML = events
                     }})
                 .catch(error => console.log(error));
-
 
         });
         console.log('Loaded!');
@@ -302,20 +189,6 @@ function page(num, lat,lng){
 
             .then(response => response.json())
             .then(data => {
-
-
-                console.log(data);
-                console.log('page clicked!')
-                var events ='';
-                console.log(data.events);
-                total = document.getElementById('total');
-                total.innerHTML = `<h2>Total Events ${data.meta.total}</h2>`
-                for(var i = 0; i < data.events.length; i++) {
-                    const date = new Date(data.events[i].datetime_local);
-                    const [year, month, day] = [date.getFullYear(), date.getMonth() + 1, date.getDate()];
-                    const dateString = 'm/d/year'+" "+`${month}/${day}/${year}`;
-                    events += `<div class='loopEvent profile-div-outer row'>${data.events[i].title}\n`+" "+`${data.events[i].venue.name}\n`+" "+`${data.events[i].venue.display_location}\n`+" Date: "+ dateString + `${data.events[i].popularity}\n`+`<div class="row"><a href="/events/${data.events[i].id}" class="col profile-btn">Host a party at ${data.events[i].title}</a>`+
-                        `<a href="/parties/${data.events[i].id}" class="col profile-btn">Or see parties at ${data.events[i].title}</a></div></div></br></br>`
 
                     console.log(data);
                     console.log('page clicked!, if statemnt')
@@ -334,7 +207,6 @@ function page(num, lat,lng){
                     let pageNum = document.getElementById('pageNum');
                     pageNum.innerHTML = "Page" +" " + (num+1)
                     document.getElementById("eventTitle").innerHTML = events
-
                 }
             ).catch(error => console.log(error));
     } else{
@@ -362,3 +234,4 @@ function page(num, lat,lng){
             ).catch(error => console.log(error));
     }
 }
+
